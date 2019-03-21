19ac11a5ad4d250301407b87fcae49564463ce0b
Omar Perozo <operozo@intelenz.com>
2019-03-21 15:20:17 -0400

    Inicio del proyecto integrado con angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: 'hola',
+})
+export class AppComponent implements OnInit {
+  title = 'gestion-costos-operativos';
+
+  ngOnInit(): void {
+  }
+}
